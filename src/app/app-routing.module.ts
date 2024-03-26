27064fad0b392faf97246c9b26759d3d91073f33
lidicedm <lidice.dominguezm@hotmail.com>
2024-03-26 00:50:53 -0500

    creación del proyecto en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { PagListaVehiculosComponent } from './paginas/pagListaVehiculos/pagListaVehiculos.component';
+import { PageNotFoundComponent } from './paginas/pageNotFound/pageNotFound.component';
+import { PagVehiculosComponent } from './paginas/pagVehiculos/pagVehiculos.component';
+import { PagRegistroVehiculoComponent } from './paginas/pagRegistroVehiculo/pagRegistroVehiculo.component';
+import { PagEditarVehiculoComponent } from './paginas/pagEditarVehiculo/pagEditarVehiculo.component';
+import { PagRegistrarClienteComponent } from './paginas/pagRegistrarCliente/pagRegistrarCliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home", 
+    component: HomeComponent
+  },
+  {
+    path: "vehiculos", 
+    component: PagListaVehiculosComponent,
+    
+  },
+  {
+    path: "clientes", 
+    component: PagRegistrarClienteComponent,
+    
+  },
+  {
+    path: "vehiculo", 
+    component: PagRegistroVehiculoComponent
+    
+  },
+  {
+    path: "vehiculo/:codigo", 
+    component: PagEditarVehiculoComponent
+  },
+  {
+    path: "vehiculos/:codigo", 
+    component: PagVehiculosComponent
+    
+  },
+  {
+    path: "", 
+    component: PagListaVehiculosComponent,
+    pathMatch:"full"
+  },
+  {
+    path: "**", 
+    component: PageNotFoundComponent,
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { paginaModule } from './paginas/paginaModule';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { UserInterceptorService } from './interceptores/userInterceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    paginaModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    {provide: HTTP_INTERCEPTORS, useClass: UserInterceptorService,multi:true}
   ],
   bootstrap: [AppComponent]
 })
