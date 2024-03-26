72eb9d07c222b1f385b41d88530dad5cc2b1c839
lidicedm <lidice.dominguezm@hotmail.com>
2024-03-26 00:57:14 -0500

    Se añade componente calificacion y el logo de la pagina

---
+++ src/app/utilitarios/componentes/calificacion/calificacion.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-calificacion',
+  templateUrl: './calificacion.component.html',
+  styleUrls: ['./calificacion.component.css']
+})
+export class CalificacionComponent implements OnInit, OnChanges {
+
+  @Input() calificacion: any = 0;
+
+  @Output() accionClick = new EventEmitter<any>();
+
+  lista: Array<any> = [];
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.generar();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['calificacion'].currentValue) {
+      this.generar();
+    }
+  }
+
+  generar() {
+    this.lista=[];
+    for (let i = 0; i < this.calificacion; i++) {
+      this.lista.push(i);
+    }
+  }
+  select() {
+    this.accionClick.emit(this.calificacion);
+  }
+
+}
